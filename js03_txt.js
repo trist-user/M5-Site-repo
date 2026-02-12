59080937612f0bfeb0515007b6753f4bcc2ec6e0
Tristan Owen <towen1@my.waketech.edu>
2026-02-12 14:08:13 -0500

    progression

--- js03_txt.js
+++ js03_txt.js
@@ -4,9 +4,70 @@
 
      Tipton Turbines
      Program to display games results in a web table
-     Author: 
-     Date:   
+     Author: tristan
+     Date:   2/12/26
 
      Filename: js03.js
  */
 
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"];
+
+window.addEventListener("load", addWeekDays);
+
+// function to write weekday names into the calender 
+function addWeekDays() {
+        let i =0; // initial counter value
+
+    //reference the collection of heading cells 
+    let headingCells = document.getElementsByTagName("th");
+
+    // write each of the seven days into a heading cell
+    while (i<7) {
+        headingCells[i].innerHTML = weekDays[i];
+
+        // increase the counter by 1
+        i++;
+
+    }
+}
+
+window.addEventListener("load", showGames);
+// Function to write game information into the calender
+function showGames(){
+    for (let i = 0; i < gameDates.length; i++){
+        let gameInfo = "";
+
+        //open the paragraph
+        gameInfo += "<p>";
+
+        //display the game location
+        if (gameLocations[i] === "h"){
+            gameInfo += "vs. ";
+        } else if (gameLocations[i] === "a") {
+            gameInfo += "@ ";
+        }
+
+        //include the opponent
+        gameInfo += gameOpponents[i] + "<br>";
+
+        //include the result and score
+        gameInfo += gameResults[i] + ": (" +runsScored[i] + "-" + runsAllowed[i] + ")";
+        
+        //Display innings played for suspended, shortened, or extrainning games
+        if (gameInnings[i] < 5) {
+            gameInfo += " [" + gameInnings[i]+"]***";
+        } else if (gameInnings [i] < 9) {
+            gameInfo += " [" + gameInnings[i] + "]*";
+        } else if (gameInnings [i] > 9) {
+            gameInfo += "[" + gameInnings [i] + "]";
+        }
+
+        //Close the paragraph
+        gameInfo += "<p>";
+
+        //write the information into a table cell
+        let tableCell = document.getElementById (gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+    };
+}
